3f26b13c0b218e929423f2af09571217e7f2fdd2
Kaustubh <kaustubh8856@gmail.com>
2024-04-14 12:31:47 +0530

    Contact&Footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Contact from "./components/Contact/contact";
+import Footer from "./components/Footer/footer";
 import Intro from "./components/Intro/intro";
 import Navbar from "./components/NavBar/navbar";
 import Skills from "./components/Skills/skills";
@@ -11,7 +12,8 @@ function App() {
       <Intro />
       <Skills />
       <Works />
-      <Contact/>
+      <Contact />
+      <Footer />
     </div>
   );
 }
--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./navbar.css";
-import logo from "../../assets/logo1.png";
+import logo from "../../assets/ll.png";
 import contactImg from "../../assets/contact.png";
 import { Link } from "react-scroll";
 
@@ -10,13 +10,60 @@ const Navbar = () => {
       <img src={logo} alt="logo" className="logo" />
 
       <div className="desktopMenu">
-        <Link className="desktopMenuListItem">Home</Link>
-        <Link className="desktopMenuListItem">About</Link>
-        <Link className="desktopMenuListItem">Portfolio</Link>
-        <Link className="desktopMenuListItem">Clients</Link>
+        <Link
+          activeClass="active"
+          to="intro"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          Home
+        </Link>
+        <Link
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          About
+        </Link>
+        <Link
+          activeClass="active"
+          to="works"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          Portfolio
+        </Link>
+        <Link
+          activeClass="active"
+          to="clients"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          Clients
+        </Link>
       </div>
 
-      <button className="desktopMenuBtn">
+      <button
+        className="desktopMenuBtn"
+        onClick={() => {
+          document
+            .getElementById("contact")
+            .scrollIntoView({ behavior: "smooth" });
+        }}
+      >
         <img src={contactImg} alt="" className="desktopMenuImg" />
         Contact Me
       </button>
